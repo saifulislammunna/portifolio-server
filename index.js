ce1c09668a7eafb5285452c3e53df36c2531a866
saifulislammunna <saifulmunna28@gmail.com>
2021-11-15 19:00:12 +0600

    server-added

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+const cors = require('cors');
+require('dotenv').config()
+
+
+const app = express()
+const port =  process.env.PORT || 5000;
+
+
+// middleware
+ app.use(cors());
+ app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u6dke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('database connected');
+        const database = client.db('portfolio');
+
+        const workCollection = database.collection('works');
+
+
+        
+         // GET works API 
+         app.get('/works', async(req, res) => {
+            const cursor = workCollection.find({});
+            const works = await cursor.toArray();
+            res.send(works);
+
+        });
+
+    }
+    finally{
+        // await client.close()
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
